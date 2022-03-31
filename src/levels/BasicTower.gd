8d5d5943948016c0effedca69501835673254513
schneediener <100398434+schneediener@users.noreply.github.com>
2022-03-31 23:23:46 +1030

    tilemap and more

--- src/levels/BasicTower.gd
+++ src/levels/BasicTower.gd
@@ -1,14 +1,26 @@
 extends KinematicBody2D
 
 var enemy_array = []
+var built = true
+var select_mode = "first"
 onready var enemy = get_node("../TestEnemy")
 
 func _physics_process(_delta):
-	track_enemy()
+#	if enemy_array.size >= 1 and built:
+#		select_enemy(select_mode)
+		track_enemy()
+
+func select_enemy(select_mode):
+	var enemy_progress_array = []
+	for i in enemy_array:
+		enemy_progress_array.append(i.offset)
+	var max_offset = enemy_progress_array.max()
+	var enemy_index = enemy_progress_array.find(max_offset)
+	enemy = enemy_array[enemy_index]
 
 func track_enemy():
 	#for the animation of the turret aiming at the enemy
-	pass
+	
 	#print (enemy)
 	self.look_at(enemy.position)
 
@@ -21,12 +33,9 @@ func fire_primary():
 func _on_Range_body_entered(body):
 	
 	enemy_array.append(body)
-	print (enemy_array)
-	pass # Replace with function body.
+
 
 
 func _on_Range_body_exited(body):
 	enemy_array.erase(body)
-	print (enemy_array)
-	print ("cleared")
-	pass # Replace with function body.
+
