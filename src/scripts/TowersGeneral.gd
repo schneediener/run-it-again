438ed44f8d2ef0b72b736628e6e46db9c938ef0a
Sean <schneediener@gmail.com>
2022-04-27 20:39:17 +0930

    orphan bullets moving themselves

--- src/scripts/TowersGeneral.gd
+++ src/scripts/TowersGeneral.gd
@@ -113,12 +113,15 @@ func _on_SelectTower_pressed():
 
 
 func fire_primary():
+	var bullet_container = get_node("../../BulletContainer")
 	if enemy and ready_to_fire:
 		
 		var bullet = load("res://src/scenes/towers/Bullet.tscn").instance()
 		bullet.damage = self.damage
 		$FacingDirection/BulletContainer.add_child(bullet, false)
+		bullet_container.add_child(bullet, false)
 		bullet.global_position = $FacingDirection/MuzzlePosition1.global_position
+		bullet.target = enemy
 #		bullet.destination = enemy.position
 		ready_to_fire = false
 		$FiringRate.start()
