59a86466ec2f45a74e0fe7f289ac4553ff3e446f
schneediener <100398434+schneediener@users.noreply.github.com>
2022-05-30 23:24:23 +0930

    basic fix for menu

--- src/scripts/MainMenu.gd
+++ src/scripts/MainMenu.gd
@@ -3,16 +3,16 @@ extends Control
 
 #I don't love this "If X != OK" working for changing scenes. 
 #it isn't natural, but, it works to get rid of warnings about unused values
-
-func _on_NewGameSean_pressed():
-	self.queue_free()
-	#get_tree().change_scene("res://src/scenes/levels/SeanMap.tscn")
-	if get_tree().change_scene("res://src/scenes/levels/SeanMap.tscn") != OK:
-		print("scene change failed")
-
-
-func _on_NewGameTest_pressed():
-	self.queue_free()
-#	get_tree().change_scene("res://src/scenes/levels/level.tscn")
-	if get_tree().change_scene("res://src/scenes/levels/level.tscn") != OK:
-		print("scene change failed")
+#
+#func _on_NewGameSean_pressed():
+#	self.queue_free()
+#	#get_tree().change_scene("res://src/scenes/levels/SeanMap.tscn")
+#	if get_tree().change_scene("res://src/scenes/levels/SeanMap.tscn") != OK:
+#		print("scene change failed")
+#
+#
+#func _on_NewGameTest_pressed():
+#	self.queue_free()
+##	get_tree().change_scene("res://src/scenes/levels/level.tscn")
+#	if get_tree().change_scene("res://src/scenes/levels/Map2.tscn") != OK:
+#		print("scene change failed")
