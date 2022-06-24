5a940b7a58a88349cf63f939a7443f28461f8621
schneediener <100398434+schneediener@users.noreply.github.com>
2022-06-25 01:50:31 +0930

    making laser area2d
    the raycast is a bit janky and feels 'light'. making changes to instead cast the laser over the top of enemies, and interate damage on them

--- src/scenes/towers/CraterCannon.gd
+++ src/scenes/towers/CraterCannon.gd
@@ -7,28 +7,60 @@ var buy_value = 150
 var muzzle_count = 1
 var upgrade_value = 450
 var sell_value = 150
-var damage = 3
+var damage = 2
 var cooldown = 0
+var ready = true
 
-
-func _ready():
-	$FacingDirection/LaserBeam2D.is_casting = false
-
-
-func _on_LaserBeam2D_enemy_hit(enemy_hit):
-	enemy_hit.take_damage(1, true)
+onready var laser_firing = false
 
 func _process(delta):
-	if $FacingDirection/LaserBeam2D.is_casting:
-		cooldown += 2.5
-		$FacingDirection.look_at(get_global_mouse_position())
-		if cooldown >= 100:
-			$FacingDirection/LaserBeam2D.is_casting = false
-			cooldown = 0
-	if Input.is_action_just_pressed("ui_cancel"):
-		if $FacingDirection/LaserBeam2D.is_casting:
-			$FacingDirection/LaserBeam2D.is_casting = false
-			cooldown = 0
-		else:
-			$FacingDirection/LaserBeam2D.is_casting = true
-			$FacingDirection.look_at(get_global_mouse_position())
+		if Input.is_action_just_pressed("ui_cancel"):
+			if self.ready:
+				$LaserTimer.start()
+				laser_firing = true
+
+func _physics_process(delta):
+	$FacingDirection.look_at(get_global_mouse_position())
+	if laser_firing:
+		do_laser_damage()
+
+func do_laser_damage():
+	for enemy in $Range.get_overlapping_bodies():
+		if enemy.type == "enemy":
+			enemy.take_damage(damage, true)
+
+
+func _on_LaserTimer_timeout():
+	laser_firing = false
+	$FiringRate.start()
+
+func _on_FiringRate_timeout():
+	self.ready = true
+
+#OLD CODE
+#func _ready():
+#	$FacingDirection/LaserBeam2D.is_casting = false
+#
+#
+#func _on_LaserBeam2D_enemy_hit(enemy_hit):
+#	enemy_hit.take_damage(1, true)
+#
+#func _process(delta):
+#	if $FacingDirection/LaserBeam2D.is_casting:
+#		cooldown += 2.5
+#		$FacingDirection.look_at(get_global_mouse_position())
+#		if cooldown >= 100:
+#			$FacingDirection/LaserBeam2D.is_casting = false
+#			cooldown = 0
+#	if Input.is_action_just_pressed("ui_cancel"):
+#		if $FacingDirection/LaserBeam2D.is_casting:
+#			$FacingDirection/LaserBeam2D.is_casting = false
+#			cooldown = 0
+#		else:
+#			$FacingDirection/LaserBeam2D.is_casting = true
+#			$FacingDirection.look_at(get_global_mouse_position())
+
+
+
+
+
