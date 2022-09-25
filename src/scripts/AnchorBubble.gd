45634ddfe8588cfa37b50243054df4d5a3dee718
Sean <schneediener@gmail.com>
2022-09-25 16:13:06 +0930

    starting on gradual slow + effect dict
    starting to conceptualise how to handle multiple slowing and speed-up effects layering over the top of each other.
    
    Doesn't currently work

--- src/scripts/AnchorBubble.gd
+++ src/scripts/AnchorBubble.gd
@@ -1,15 +1,21 @@
 extends Node2D
 var freeze_active = true
+var max_slow = -0.5
 
 func _process(delta):
 	var curr_bodies = $Area2D.get_overlapping_bodies()
+	
 	if freeze_active:
 		for body in curr_bodies:
-			body.time_freeze = true
+			var factor = (body.distance_to(self)/self.radius)
+			var total_slow = max_slow*factor
+			if !body.effect_dict[self.get_instance_id()]:
+				body.effect_dict[self.get_instance_id()] = [total_slow]
+				#hmmmmmmm
 func expire_bubble():
 	var curr_bodies = $Area2D.get_overlapping_bodies()
 	for body in curr_bodies:
-		body.time_freeze = false
+		body.temporal_momentum += body.distance_to(self)/self.radius
 	freeze_active = false
 	self.queue_free()
 func _on_Timer_timeout():
