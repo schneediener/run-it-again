60242b1a623d0a7a33a090e8f909efe4c22851ae
Sean <schneediener@gmail.com>
2022-10-13 17:47:40 +1030

    animated sprite

--- src/scenes/DialogueRoot.gd
+++ src/scenes/DialogueRoot.gd
@@ -12,12 +12,16 @@ onready var current_text = $TextDialogue
 func _process(delta):
 	current_text.visible_characters += 1
 	if current_text.percent_visible < 0.95:
+		$CharacterBorder/CharacterDialogue.playing = true
 		var pitch_int = randi() % 2
 		if pitch_int == 0:
 			$AudioStreamPlayer2D.pitch_scale = 0.8
 		else:
 			$AudioStreamPlayer2D.pitch_scale = 0.4
 		$AudioStreamPlayer2D.play()
+	else:
+		$CharacterBorder/CharacterDialogue.playing = false
+		$CharacterBorder/CharacterDialogue.frame = 0
 
 
 func _on_ButtonNext_pressed():
