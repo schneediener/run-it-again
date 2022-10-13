ce8bebb8e1fac14938e62a80e56bf5a4557fcaaa
Sean <schneediener@gmail.com>
2022-10-13 17:35:44 +1030

    basic dialogue sounds

--- src/scenes/DialogueRoot.gd
+++ src/scenes/DialogueRoot.gd
@@ -11,6 +11,13 @@ onready var current_text = $TextDialogue
 
 func _process(delta):
 	current_text.visible_characters += 1
+	if current_text.percent_visible < 0.95:
+		var pitch_int = randi() % 2
+		if pitch_int == 0:
+			$AudioStreamPlayer2D.pitch_scale = 0.8
+		else:
+			$AudioStreamPlayer2D.pitch_scale = 0.4
+		$AudioStreamPlayer2D.play()
 
 
 func _on_ButtonNext_pressed():
