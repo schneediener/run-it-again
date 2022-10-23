d002137c72d3adaeb482045ec6d4c4001cbed659
Sean <schneediener@gmail.com>
2022-10-23 12:08:19 +1030

    text clickable and dialogue works

--- src/scripts/DecisionMenu.gd
+++ src/scripts/DecisionMenu.gd
@@ -1,6 +1,11 @@
 extends PanelContainer
 
 
+onready var current_text = $MarginContainer/RootVBox/CharacterHBox/PanelDialogue/MarginContainer/MarginContainer/TextDialogue
+var playing = false
+onready var character_scene = get_node("MarginContainer/RootVBox/CharacterHBox/PortraitContainer")
+onready var character_sprite = character_scene.get_node("CharacterControl/CharacterSprite")
+onready var audio_player = $MarginContainer/RootVBox/CharacterHBox/PanelDialogue/AudioStreamPlayer2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,15 +13,28 @@ extends PanelContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	current_text.visible_characters = 0
+
+func _process(delta):
+	if Input.is_action_pressed("ig_space"):
+		playing = true
+#		_on_ButtonNext_pressed()
+	if playing:
+		current_text.visible_characters += 1
+		if current_text.percent_visible < 0.95:
+			character_sprite.playing = true
+			var pitch_int = randi() % 2
+			if pitch_int == 0:
+				audio_player.pitch_scale = 0.8
+			else:
+				audio_player.pitch_scale = 0.4
+			audio_player.play()
+		else:
+			character_sprite.playing = false
+			character_sprite.frame = 0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Choice1_gui_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and event.pressed:
-				pass
+func _on_ButtonNext_pressed():
+	current_text.visible = false
+	current_text = $MarginContainer/RootVBox/CharacterHBox/PanelDialogue/MarginContainer/MarginContainer/TextDialogue2
+	current_text.visible = true
