b71a483a0234d040f7101abe23e5dd2884ca402e
Sean <schneediener@gmail.com>
2022-11-13 16:10:47 +1030

    ? more

---
+++ src/scenes/enemies/new_ContextEnemy.gd
@@ -0,0 +1,6 @@
+extends "res://src/scripts/EnemyMovement.gd"
+
+var max_health = 10
+var gold_multi = 1
+var speed = 250
+var steer_force = 0.1
--- src/scripts/test_enemy1.gd
+++ src/scripts/test_enemy1.gd
@@ -1,4 +1,4 @@
-extends "res://src/scripts/test_enemies_general.gd"
+extends EnemyGeneral
 
 var speed = 310
 var max_health = 10
